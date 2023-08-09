82c44f3e48b677e603115c4df31a0b98c31448e8
Xupeng Zhu <xzhu@theaiinstitute.com>
2023-08-09 13:48:14 -0400

    tuned YOH O

---
+++ src/openhand_node/Model_O_test.py
@@ -0,0 +1,12 @@
+from src.openhand_node.hands import Model_O
+
+if __name__ == '__main__':
+    T = Model_O('/dev/ttyUSB0', 4, 1, 2, 3, 'XM', 0.0, 0.36, -0.11, 0.05)
+    T.readHand()
+    T.readLoads()
+    # T.readMotorMins()
+    T.moveMotor()
+    T.pinch_close()
+    # T.close(0.5)
+    # T.reset()
+    # T.release()
